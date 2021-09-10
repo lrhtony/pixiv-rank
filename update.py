b1945da3f9a5690e7a5da15e1f801bc9629826c0
lrhtony <tony07017@163.com>
2021-09-10 22:56:11 +0800

    通过测试图片链接来确定图片格式，稳定性还有待测试

--- update.py
+++ update.py
@@ -5,6 +5,11 @@ import json
 request_list = []
 info_list = []
 json_dict = {}
+headers = {
+    'Referer': 'https://www.pixiv.net/',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.31 Safari/537.36 Edg/94.0.992.14'
+}
+
 for pages in range(1, 11):
     request_list.append(grequests.get('https://www.pixiv.net/ranking.php?mode=daily&content=illust&p=%s&format=json'
                                       % (str(pages))))
@@ -12,15 +17,28 @@ for pages in range(1, 11):
 response_list = grequests.map(request_list)
 
 for response in response_list:
+    info_list_part = []
+    format_test_list = []
     content = response.json()['contents']
     for i in content:
         information = {'title': i['title'], 'illust_page_count': i['illust_page_count'], 'illust_id': i['illust_id'],
                        'user_name': i['user_name'], 'user_id': i['user_id'], 'width': i['width'], 'height': i['height'],
                        'rank': i['rank'], 'illust_upload_timestamp': i['illust_upload_timestamp'],
                        'illust_content_type': i['illust_content_type'],
-                       'url': i['url']}
-        info_list.append(information)
+                       'preview_url': i['url']}
+        info_list_part.append(information)
+        format_test_list.append(grequests.head(i['url'].replace('/c/240x480/img-master/', '/img-original/')
+                                               .replace('_master1200', ''), headers=headers))
 
+    format_response_list = grequests.map(format_test_list)
+    for j in range(len(format_response_list)):
+        if format_response_list[j].status_code == 404:
+            info_list_part[j]['url'] = format_response_list[j].url.replace('.jpg', '.png')
+        elif format_response_list[j].status_code == 200:
+            info_list_part[j]['url'] = format_response_list[j].url
+        else:
+            print(info_list_part[j]['url'] + 'error')
+    info_list = info_list + info_list_part
 
 json_dict['contents'] = info_list
 json_dict['date'] = response_list[0].json()['date']
