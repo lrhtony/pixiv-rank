2d11647ed4e2a50815a3e26af191fe71ac172a93
lrhtony <tony07017@163.com>
2021-09-12 22:37:37 +0800

    完善api

--- api/random-pic.py
+++ api/random-pic.py
@@ -1,32 +1,39 @@
 # -*- coding: utf-8 -*-
-from flask import Flask, request, jsonify
-import requests
+from flask import Flask, request, jsonify, redirect
 import random
 import json
+from urllib import parse
 
 app = Flask(__name__)
 
-'''Here is the routes part!'''
+'''---------------------------------------------下面是主入口---------------------------------------------'''
 
 
 @app.route('/api/random-pic', methods=['GET'])
 def random_picture_route():
     pic_type = request.args.get('type')
     enable_sex = request.args.get('sex')
-    if pic_type != 'pc' and pic_type != 'phone' and pic_type != 'square' and pic_type != 'all':
+    return_format = request.args.get('format')
+    proxy = request.args.get('proxy')
+    if proxy is None:  # 设置图片返回url的代理
+        proxy = 'https://i.pixiv.cat/'
+    proxy = parse.unquote(proxy)
+    if pic_type != 'pc' and pic_type != 'phone' and pic_type != 'square' and pic_type != 'all':  # 图片样式
         pic_type = 'all'
-    if enable_sex == 'True' or enable_sex == 'true' or enable_sex == '1':
+    if enable_sex == 'True' or enable_sex == 'true' or enable_sex == '1':  # 处理那些sex=1的图片，决定是否屏蔽
         enable_sex = 1
     else:
         enable_sex = 0
-    pic_info = random_picture(pic_type, enable_sex)
-    return jsonify(pic_info)
+    if return_format != 'json' and return_format != 'raw':  # 返回信息样式，raw则重定向到图片
+        return_format = 'json'
+    pic = random_picture(pic_type, enable_sex, return_format, proxy)
+    return pic
 
 
-'''Here is the logical part!'''
+'''---------------------------------------------下面是主函数部分---------------------------------------------'''
 
 
-def random_picture(pic_type: str, enable_sex: int):
+def random_picture(pic_type: str, enable_sex: int, return_format: str, proxy: str):
     with open('daily.json', 'r', encoding='utf-8') as file:
         contents = json.loads(file.read())['contents']
     while True:
@@ -43,13 +50,13 @@ def random_picture(pic_type: str, enable_sex: int):
             break
 
     illust_id = content['illust_id']
-    illust_page_count = int(content['illust_page_count'])
+    print(illust_id)
     illust_upload_timestamp = content['illust_upload_timestamp']
     rank = content['rank']
     title = content['title']
     user_id = content['user_id']
     user_name = content['user_name']
-    url = content['url'].replace('i.pximg.net', 'i.pixiv.cat')
+    url = content['url'].replace('https://i.pximg.net/', proxy)
     pic_info = {
         'illust_id': illust_id,
         'url': url,
@@ -59,7 +66,13 @@ def random_picture(pic_type: str, enable_sex: int):
         'user_name': user_name,
         'upload_timestamp': illust_upload_timestamp
     }
-    return pic_info
+    if return_format == 'json':
+        return jsonify(pic_info)
+    elif return_format == 'raw':
+        del pic_info['title']
+        del pic_info['user_name']
+        del pic_info['url']
+        return redirect(url, 307), 307, pic_info
 
 
 if __name__ == '__main__':
